981999473c649c1ff9976cd8cd84c4277410ee34
eXIA008 <165605674+eXIA008@users.noreply.github.com>
2026-04-26 13:45:03 +0700

    feat: add Documents model and migration

---
+++ readfolio/app/Models/Documents.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Documents extends Model
+{
+    use HasFactory, HasUuids;
+    protected $table = 'documents';
+    const UPDATED_AT = 'last_read_at';
+    protected $fillable = [
+        'user_id',
+        'title',
+        'filename',
+        'last_page',
+        'last_read_at'
+    ];
+
+    protected $appends = [
+        'file_url'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'last_read_at' => 'datetime',
+    ];
+
+    public function getFileUrlAttribute(): string
+    {
+        return "/static/uploads/{$this->filename}";
+        
+        // Catatan: Jika Anda ingin URL lengkap termasuk domain (http://...), 
+        // Anda bisa menggunakan helper asset() bawaan Laravel seperti ini:
+        // return asset("static/uploads/{$this->filename}");
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
